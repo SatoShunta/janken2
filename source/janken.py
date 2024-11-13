4a5485c914f7e48153c0d64ed65d01d90153cadd
yuzechihiro <c.yuze.sys24@morijyobi.ac.jp>
2024-11-13 15:53:01 +0900

    完成

--- source/janken.py
+++ source/janken.py
@@ -1,19 +1,24 @@
-import random
 import player
 import computer
 import janken_judge
 # ユーザの手
 U_hand = -1
 # PCの手
-P_hand = [1,2,3]
+P_hand = -1
 win = 0
 lose = 0
 Tie = 0
 
 for _ in range(3):
-    player.pon()
-    computer.P_hand()
-    janken_judge.judge()
+    U_hand = player.user_pon()
+    P_hand = computer.cpu_pon()
+    judge = janken_judge.judge(U_hand,P_hand)
+    if judge == 'win':
+        win += 1
+    elif judge == 'lose':
+        lose += 1
+    elif judge == 'Tie':
+        Tie += 1
 
 print("【最終結果】")
 if win > lose:
--- source/player.py
+++ source/player.py
@@ -1,7 +1,7 @@
 # ユーザの手
 U_hand = -1
 
-def pon():
+def user_pon():
     while(True):
         U_hand = int(input('1.グー 2.チョキ 3.パー\nグー、チョキ、パーのいずれかを入力してください＞'))
         if(U_hand == 1 or U_hand == 2 or U_hand == 3):
@@ -11,9 +11,7 @@ def pon():
                 print(f'あなたの手：チョキ')
             else:
                 print(f'あなたの手：パー')
+            return U_hand
             break
         else:
             print('1,2,3のどれかを入力してください')
-            
-pon()
-
